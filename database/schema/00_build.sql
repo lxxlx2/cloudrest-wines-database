cc5c9933851c63e14b2c78aee30261142eed1a3b
lxxlx2 <128774529+lxxlx2@users.noreply.github.com>
2026-08-17 02:46:42 +0700

    Include validation routines in schema build

--- database/schema/00_build.sql
+++ database/schema/00_build.sql
@@ -3,3 +3,4 @@ SOURCE database/schema/01_tables.sql;
 SOURCE database/schema/02_triggers.sql;
 SOURCE database/schema/03_reporting.sql;
 SOURCE database/schema/04_final_controls.sql;
+SOURCE database/schema/05_validation_routines.sql;
